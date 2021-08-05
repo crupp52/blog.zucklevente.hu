c3ba96b5ac6cae7811e608df8de1140a52b9536e
Levente Zuck <zucklevente@gmail.com>
2021-08-05 11:10:16 +0200

    Install tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title></title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>

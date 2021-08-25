e40852b16901761b0e3df4428d57a04ebbcaffba
Levente Zuck <zucklevente@gmail.com>
2021-08-25 10:38:38 +0200

    Generate PostImage livewire component

---
+++ app/Http/Livewire/Admin/PostImages.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class PostImages extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.post-images');
+    }
+}
